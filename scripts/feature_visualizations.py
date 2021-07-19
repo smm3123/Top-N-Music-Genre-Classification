c7a7af67bb97de78abcfbfc130367e02dfcc293e
smm3123 <syedmahdi3123@gmail.com>
2021-07-18 19:55:38 -0500

    Add test audio files and input loop to script

--- scripts/feature_visualizations.py
+++ scripts/feature_visualizations.py
@@ -34,38 +34,68 @@ def main():
     """
     Retrieves relative path for audio file and outputs various visualizations
     """
-    audio_file = load_audio_file()
 
-    # Set figure dimensions and display feature visualizations
+    # Keep looping until user opts to exit the program
+    while True:
+        user_input = get_user_input()
+        if user_input == 1:
+            audio_file = AudioFile('blues.00001.wav')
+        elif user_input == 2:
+            audio_file = AudioFile('country.00001.wav')
+        elif user_input == 3:
+            audio_file = AudioFile('jazz.00001.wav')
+        elif user_input == 4:
+            audio_file = load_custom_audio_file()
+        else:
+            break
+
+        display_feature_visualizations(audio_file)
+
+
+def get_user_input():
+    """
+    :return: User input as an integer value
+    """
+    return int(input("Choose an option from the following genres to visualize:"
+                     "\n1. Blues\n2. Country\n3. Jazz\n4. Custom .wav audio file\n5. Exit\n"))
+
+
+def load_custom_audio_file():
+    """
+    :return: AudioFile object based off of file from user-inputted relative path
+    """
+    path = input("Enter the relative path of the .wav audio file: ")
+    audio_file = AudioFile(path)
+    return audio_file
+
+
+def display_feature_visualizations(audio_file):
+    """
+    Sets figure dimensions and displays feature visualizations
+    """
     plt.figure(figsize=(14, 5))
     display_waveplot(audio_file)
     display_spectrogram(audio_file)
     display_mel_spectrogram(audio_file)
 
 
-def load_audio_file():
-    """
-    :return: AudioFile object based off of file from user-inputted relative path
-    """
-    path = input("Enter the relative path of the audio file: ")
-    audio_file = AudioFile(path)
-    return audio_file
-
-
 def display_waveplot(audio_file):
     librosa.display.waveplot(audio_file.audio_time_series, sr=audio_file.sampling_rate)
+    plt.title("Waveform")
     plt.show()
 
 
 def display_spectrogram(audio_file):
     librosa.display.specshow(audio_file.db_spectrogram, sr=audio_file.sampling_rate, x_axis='time', y_axis='hz')
     plt.colorbar()
+    plt.title("Spectrogram")
     plt.show()
 
 
 def display_mel_spectrogram(audio_file):
     librosa.display.specshow(audio_file.mel_spectrogram, y_axis='log', x_axis='time')
     plt.colorbar()
+    plt.title("Mel Spectrogram")
     plt.show()
 
 
