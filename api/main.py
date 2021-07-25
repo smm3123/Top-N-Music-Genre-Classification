64dc474405bd3ab4a047420e0a5454de7c06914f
Andrew Brown <Ahbrown37@gmail.com>
2021-07-25 14:14:09 -0500

    restructured file directory created 2 yaml files. one for frontend 'frontend' one for backend 'api' created a python flask server with endpoints for the two youtube helper functions

--- api/main.py
+++ api/main.py
@@ -3,8 +3,11 @@ import json
 import os
 from dotenv import load_dotenv
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__, static_folder="../build", static_url_path='/')
+CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 
 @app.route('/')
