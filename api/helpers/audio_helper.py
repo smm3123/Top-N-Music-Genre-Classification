2f49dc8cf336e39c2182639a3ea1a1d4121fe3ce
smm3123 <syedmahdi3123@gmail.com>
2021-08-01 03:44:56 -0500

    Comment out unused variable for linter

--- api/helpers/audio_helper.py
+++ api/helpers/audio_helper.py
@@ -8,5 +8,5 @@ def get_genres(youtube_url):
     """
     file_id = youtube_helper.download_audio_from_youtube(youtube_url)
     file_name = file_id + '.m4a'
-    audio_file = AudioFile('temp/' + file_name)
+    # audio_file = AudioFile('temp/' + file_name)
     is_delete_successful = youtube_helper.delete_audio_file(file_name)
