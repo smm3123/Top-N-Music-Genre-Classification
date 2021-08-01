9e262cbcd47a443fde6a663d6d5b0c6a0ac10c7f
smm3123 <syedmahdi3123@gmail.com>
2021-08-01 03:46:58 -0500

    Resolve linter complaints

--- api/helpers/audio_helper.py
+++ api/helpers/audio_helper.py
@@ -1,5 +1,4 @@
 from helpers import youtube_helper
-from models.audio_file import AudioFile
 
 
 def get_genres(youtube_url):
@@ -8,5 +7,4 @@ def get_genres(youtube_url):
     """
     file_id = youtube_helper.download_audio_from_youtube(youtube_url)
     file_name = file_id + '.m4a'
-    # audio_file = AudioFile('temp/' + file_name)
-    is_delete_successful = youtube_helper.delete_audio_file(file_name)
+    youtube_helper.delete_audio_file(file_name)
