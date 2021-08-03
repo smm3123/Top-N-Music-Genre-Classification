6bc8211e161eaae0c31b1249818e5beef77c3b5c
ja-osu <47234604+ja-osu@users.noreply.github.com>
2021-08-02 20:23:15 -0400

    Added neural network scripts
    Uploaded the model, the scripts to create a dataset as a JSON file, training the neural network, and make predictions on new audio files.

--- scripts/feature_extraction_dataset.py
+++ scripts/feature_extraction_dataset.py
@@ -4,36 +4,13 @@ import json
 import os
 
 
-DATASET_DIR_PATH = "Relative/Path/to/dataset/directory"
-JSON_FILE_PATH = "dataset.json"
-
-
-class AudioFile:
-    """
-    Class containing relevant data about the audio file
-    """
-
-    def __init__(self, path):
-        self.audio_time_series, self.sampling_rate = librosa.load(path)
-        self.db_spectrogram = self._get_db_scaled_spectrogram()
-        self.mel_spectrogram = self._get_mel_spectrogram()
-
-    def _get_db_scaled_spectrogram(self):
-        """
-        Converts an amplitude spectrogram to dB-scaled spectrogram.
-        :return: np.ndarray of the dB-scaled spectrogram
-        """
-        stft = librosa.stft(self.audio_time_series)
-        db_spectrogram = librosa.amplitude_to_db(np.abs(stft))
-        return db_spectrogram
-
-    def _get_mel_spectrogram(self):
-        """
-        :return: np.ndarray of mel spectrogram
-        """
-        mel_spectrogram = librosa.feature.melspectrogram(
-            y=self.audio_time_series, sr=self.sampling_rate)
-        return librosa.power_to_db(mel_spectrogram, ref=np.max)
+DATASET_DIR_PATH = "relative/path/to/folder"
+JSON_FILE_PATH = "filename.json"
+SAMPLE_RATE = 22050
+TRACK_LENGTH = 30  # Length of the track measured in seconds
+TOTAL_SAMPLES = SAMPLE_RATE * TRACK_LENGTH
+NUM_SEGMENTS = 10
+NUM_SAMPLES_PER_SEGMENT = int(TOTAL_SAMPLES / NUM_SEGMENTS)
 
 
 def main():
@@ -41,25 +18,9 @@ def main():
     Process the dataset audio files to save their mel spectograms and
     ground truth labels to create a JSON file that stores their processed
     information so that the dataset will not need to be processed every
-    time it is used.
-    """
-    save_mel_spectogram(DATASET_DIR_PATH, JSON_FILE_PATH)
-
-
-def load_audio_file(filename):
-    """
-    :return: AudioFile object based off of file given
-    """
-    audio_file = AudioFile(filename)
-    return audio_file
-
-
-def save_mel_spectogram(dataset_dir_path, json_file_path):
-    """
-    Processes the dataset audio files to get their mel spectogram features
-    in the form of an np.ndarray.  These mel spectogram features as well as
-    the ground truth label for the audio file are saved in lists in a JSON
-    file format.
+    time it is used.  Each song will be split into 10 segments to
+    increase number of training examples for the neural network and to
+    decrease the melspectrogram array produced.
     """
 
     genre_label_dict = {
@@ -73,26 +34,37 @@ def save_mel_spectogram(dataset_dir_path, json_file_path):
                     "mel_spectogram_features": []
                  }
 
-    for (dirpath, dirname, filenames) in os.walk(dataset_dir_path):
+    for (dirpath, dirname, filenames) in os.walk(DATASET_DIR_PATH):
 
-        if dirpath is not dataset_dir_path:
+        if dirpath is not DATASET_DIR_PATH:
 
             ground_truth_label = dirpath.split("\\")[-1]
 
             for f in filenames:
 
-                audio_file = load_audio_file(os.path.join(dirpath, f))
+                audio_time_series, sampling_rate = librosa.load(
+                    os.path.join(dirpath, f))
 
-                if audio_file.mel_spectrogram.shape == (128, 1293):
-                    audio_data["genre_label"].append(
-                        genre_label_dict[ground_truth_label])
-                    audio_data["mel_spectogram_features"].append(
-                        audio_file.mel_spectrogram.tolist())
-                else:
-                    print(f"File failed: {ground_truth_label}, "
-                          f"Shape is {audio_file.mel_spectrogram.shape}")
+                for i in range(NUM_SEGMENTS):
 
-    with open(json_file_path, 'w') as fp:
+                    start_time = NUM_SAMPLES_PER_SEGMENT * i
+                    finish_time = start_time + NUM_SAMPLES_PER_SEGMENT
+
+                    mel_spectrogram = librosa.feature.melspectrogram(
+                        audio_time_series[start_time:finish_time],
+                        sampling_rate
+                    )
+
+                    if mel_spectrogram.shape == (128, 130):
+                        audio_data["genre_label"].append(
+                            genre_label_dict[ground_truth_label])
+                        audio_data["mel_spectogram_features"].append(
+                            mel_spectrogram.tolist())
+                    else:
+                        print(f"File failed: {ground_truth_label}, "
+                              f"Shape is {mel_spectrogram.shape}")
+
+    with open(JSON_FILE_PATH, 'w') as fp:
         json.dump(audio_data, fp)
 
 
