e75fa4219c24de193e4ac2c89a9a8046a415e924
smm3123 <syedmahdi3123@gmail.com>
2021-08-07 02:28:34 -0500

    Remove extra space for linter check

--- api/helpers/audio_helper.py
+++ api/helpers/audio_helper.py
@@ -13,7 +13,7 @@ def get_genres(youtube_url):
     Downloads audio file, processes it, and returns genre classifications
     """
     file_id = youtube_helper.download_audio_from_youtube(youtube_url)
-    file_name =  f'{file_id}.m4a'
+    file_name = f'{file_id}.m4a'
     file_path = f'temp/{file_name}'
     # audio_file = AudioFile(file_path, False)
     classifier_input = AudioFile(file_path, True).mel_spectrogram  # Gets Mel spectrogram data for 3 second clip
